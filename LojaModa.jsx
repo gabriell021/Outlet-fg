1b8b3b05028a3ffb333311d958b52acb8019ff3d
gabriell021 <josimarrs0928@gmail.com>
2025-04-12 15:31:37 -0300

    Add files via upload

---
+++ LojaModa.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function LojaModa() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Bem-vindo à Outlet FG</h1>
+      <p>Roupas, calçados, acessórios e perfumes para todas as idades.</p>
+    </div>
+  );
+}
